51aee3e7e4a4ca9cc33dc96a3078fef4608d2c63
masanao.matsubara <m_matsubara@okwave.co.jp>
2019-06-08 06:46:00 +0900

    simd add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{f64, mem};
+
+#[cfg(target_arch = "x86_64")]
+use std::arch::x86_64::*;
+
+#[cfg(target_arch = "x86")]
+use std::arch::x86::*;
+
 fn main() {
+    unsafe {
+        let a = _mm256_set_pd(1.0, 2.0, 3.0, 4.0);
+    }
     println!("Hello, world!");
 }
